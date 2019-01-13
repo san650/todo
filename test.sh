494437d5e9582161e6caa413e6fcf437a42ca3d7
Santiago Ferreira <san650@gmail.com>
2019-01-12 22:44:34 -0300

    Allow to run one test at a time

--- test.sh
+++ test.sh
@@ -2,18 +2,23 @@
 
 # TAP = Test Anything Protocol http://testanything.org/
 
+TEST_INDEX="$1"
 STATUS_EXIT=0
 TMP=
 TMP_OUTPUT=
-TODO="./bin/todo"
+TODO="$(pwd)/bin/todo"
 TEST_COUNTER=0
 CURRENT_TEST=
-CURRENT_EXPECTED_OUTPUT=
-CURRENT_ACTUAL_OUTPUT=
+EXPECTED_OUTPUT=
+ACTUAL_OUTPUT_FILE_FILE=
 
 function plan {
   # Count the numbers of `spec` calls in the file
   local TOTAL=$(grep --count "^spec " "$0")
+
+  if [ -n "$TEST_INDEX" ]; then
+    TOTAL=1
+  fi
 
   echo "1..${TOTAL}"
 }
@@ -31,19 +36,19 @@ trap cleanup EXIT
 
 function tap_ok()
 {
-  echo "ok $TEST_COUNTER $1"
+  echo "ok $1 $2"
 }
 
 function tap_not_ok()
 {
-  echo "not ok $TEST_COUNTER $1"
+  echo "not ok $1 $2"
 
   # print diagnostics
   echo "#      EXPECTED:"
-  echo "$2" | sed 's/^/#      /'
+  echo "$3" | sed 's/^/#      /'
   echo "#"
   echo "#      ACTUAL:"
-  echo "$3" | sed 's/^/#      /'
+  echo "$4" | sed 's/^/#      /'
 
   STATUS_EXIT=1
 }
@@ -56,33 +61,57 @@ function finish()
 function spec {
   ((TEST_COUNTER++))
 
+  # If test index is set, skipt the test if it's not the right index
+  # This allows to run one test at a time: `./test.sh 3`
+  if [ -n "$TEST_INDEX" -a "$TEST_INDEX" != "$TEST_COUNTER" ]; then
+    return
+  fi
+
   TMP=$(mktemp -d -t todo-test-XXXXXXX)
   TMP_OUTPUT="$(mktemp -d -t todo-test-output-XXXXXXX)/output"
   CURRENT_TEST="$1"
-  CURRENT_EXPECTED_OUTPUT="$TMP/test-$TEST_COUNTER-expected-output"
-  CURRENT_ACTUAL_OUTPUT="$TMP_OUTPUT/test-$TEST_COUNTER-actual-output"
+  EXPECTED_OUTPUT="$TMP/test-$TEST_COUNTER-expected-output"
+  ACTUAL_OUTPUT_FILE_FILE="$TMP_OUTPUT/test-$TEST_COUNTER-actual-output"
   mkdir -p "$TMP_OUTPUT"
 }
 
 function expect {
   local TEST=$(cat)
 
+  # If test index is set, skipt the test if it's not the right index
+  # This allows to run one test at a time: `./test.sh 3`
+  if [ -n "$TEST_INDEX" -a "$TEST_INDEX" != "$TEST_COUNTER" ]; then
+    return
+  fi
+
   # Cleanup all variables before running a test
   TODO_PATH=
   TODO_PROJECT=
   TODO_FILTER=
 
-  eval "$TEST" 2>&1 > $CURRENT_ACTUAL_OUTPUT
+  eval "$TEST" 2>&1 > $ACTUAL_OUTPUT_FILE_FILE
 }
 
 function to_output {
-  local TEST_EXPECTED=$(cat)
-  local TEST_ACTUAL=$(cat $CURRENT_ACTUAL_OUTPUT)
+  local EXPECTED=$(cat)
+  local ACTUAL=$(cat $ACTUAL_OUTPUT_FILE_FILE)
+  local NUMBER=${TEST_COUNTER}
 
-  if [ "$TEST_EXPECTED" == "$TEST_ACTUAL" ]; then
-    tap_ok "$CURRENT_TEST"
+  # If test index is set, skipt the test if it's not the right index
+  # This allows to run one test at a time: `./test.sh 3`
+  if [ -n "$TEST_INDEX" ]; then
+    if [ "$TEST_INDEX" != "$TEST_COUNTER" ]; then
+      return
+    fi
+
+    # if test index is defined, only one test is going to run
+    NUMBER=1
+  fi
+
+  if [ "$EXPECTED" == "$ACTUAL" ]; then
+    tap_ok "$NUMBER" "$CURRENT_TEST"
   else
-    tap_not_ok "$CURRENT_TEST" "$TEST_EXPECTED" "$TEST_ACTUAL"
+    tap_not_ok "$NUMBER" "$CURRENT_TEST" "$EXPECTED" "$ACTUAL"
   fi
 }
 
